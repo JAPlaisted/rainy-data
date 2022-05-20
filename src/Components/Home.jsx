155335c7f6da5e61ae9963cd8eb23bc286ff4c95
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-05-20 15:34:11 -0700

    Rain effect

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import Guy from "../Assets/umbrellaMan.png";
 import Landscape from "../Assets/bkgrnd.png";
+import $ from "jquery";
 
 function Home() {
 
-    
+    const makeItRain = function() {
+
+        $('.rain').empty();
+      
+        let increment = 0;
+        let drops = "";
+        let backDrops = "";
+      
+        while (increment < 100) {
+          //couple random numbers to use for various randomizations
+          //random number between 98 and 1
+          const randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+
+          const randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+
+          increment += randoFiver;
+          //add in a new raindrop with various randomizations to certain CSS properties
+          drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+          backDrops += '<div class="drop" style="right: ' + increment  + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+        }
+      
+        $('.rain.front-row').append(drops);
+        $('.rain.back-row').append(backDrops);
+      }
+      
+      makeItRain();
 
     const sectionStyle = {
         backgroundImage: `url(${Landscape})`,
@@ -36,6 +62,7 @@ function Home() {
             
           </p>
         </div>
+
         <img
           className="guy"
           src={Guy}
@@ -43,6 +70,8 @@ function Home() {
           width="300px"
           height="300px"
         />
+        <div className="rain front-row"></div>
+        <div className="rain back-row"></div>
       </div>
      );
 }
