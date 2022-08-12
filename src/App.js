d3e8230d4d0de40b78f833e8c72f9d4ea3711679
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-08-11 17:22:39 -0700

    Added Browser Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Home from "./Components/Home";
+import FAQ from "./Components/FAQ";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/faq" element={<FAQ />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
